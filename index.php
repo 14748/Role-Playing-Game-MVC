66cf7be3edc689fc7c41f818446da5e2681df151
14748 <sproplayerxd@gmail.com>
2023-10-24 18:55:29 +0200

    feat(persistence): add db connection and Creature DAO
    This commit adds a PersistentManager which will establish the connection to the db and CreatureDAO which will execute querries through the db connection (Insert, Delete, Update, Select)

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+require_once __DIR__ . "/persistence/DAO/CreatureDAO.php";
+
+$creatureDAO = new CreatureDAO();
+
+$creatures = $creatureDAO->selectAll();
+foreach ($creatures as $creature) {
+    echo $creature->getName() . "</br>";
+
+    $creature->setName("Pepito1116565");
+    $creatureDAO->update($creature);
+    $creatureDAO->delete($creature);
+    //$creature->setIdCreature($creature->getIdCreature() + 1);
+    //$creatureDAO->insert($creature);
+}
---
+++ persistence/DAO/CreatureDAO.php
@@ -0,0 +1,83 @@
+<?php
+require_once  __DIR__ . '/../conf/PersistentManager.php';
+require_once __DIR__ . '/../../app/models/Creature.php';
+
+class CreatureDAO
+{
+    const CREATURE = 'creature';
+    private $conn = null;
+    public function __construct()
+    {
+        $this->conn = PersistentManager::getInstance()->get_connection();
+    }
+
+    public function selectAll()
+    {
+        $query = "SELECT * FROM " . CreatureDAO::CREATURE;
+        $result = mysqli_query($this->conn, $query);
+        $creatures = array();
+        while ($row = mysqli_fetch_array($result)) {
+
+            $creature = new Creature();
+            $creature->setIdCreature($row["idCreature"]);
+            $creature->setName($row["name"]);
+            $creature->setDescription($row["description"]);
+            $creature->setAvatar($row["avatar"]);
+            $creature->setAttackPower($row["attackPower"]);
+            $creature->setLifeLevel($row["lifeLevel"]);
+            $creature->setWeapon($row["weapon"]);
+
+            array_push($creatures, $creature);
+        }
+        return $creatures;
+    }
+
+    public function insert($creature)
+    {
+        $query = "INSERT INTO " . CreatureDAO::CREATURE .
+            " (idCreature, name, description, avatar, attackPower, lifeLevel, weapon) VALUES(?, ?, ?, ?, ?, ?, ?)";
+        $stmt = mysqli_prepare($this->conn, $query);
+
+        $idCreature = $creature->getIdCreature();
+        $name = $creature->getName();
+        $description = $creature->getDescription();
+        $avatar = $creature->getAvatar();
+        $attackPower = $creature->getAttackPower();
+        $lifeLevel = $creature->getLifeLevel();
+        $weapon = $creature->getWeapon();
+
+        mysqli_stmt_bind_param($stmt, 'isssiis', $idCreature, $name, $description, $avatar, $attackPower, $lifeLevel, $weapon);
+
+        return $stmt->execute();
+    }
+
+    public function update($creature)
+    {
+        $query = "UPDATE " . CreatureDAO::CREATURE .
+            " SET name=?, description=?, avatar=?, attackPower=?, lifeLevel=?, weapon=?"
+            . " WHERE idCreature=?";
+        $stmt = mysqli_prepare($this->conn, $query);
+        $name = $creature->getName();
+        $description = $creature->getDescription();
+        $avatar = $creature->getAvatar();
+        $attackPower = $creature->getAttackPower();
+        $lifeLevel = $creature->getLifeLevel();
+        $weapon = $creature->getWeapon();
+        $idCreature = $creature->getIdCreature();
+        mysqli_stmt_bind_param($stmt, 'sssiisi', $name, $description, $avatar, $attackPower, $lifeLevel, $weapon, $idCreature);
+        return $stmt->execute();
+    }
+
+    public function delete($creature)
+    {
+        $query = "DELETE FROM " . CreatureDAO::CREATURE . " WHERE idCreature =?";
+        $stmt = mysqli_prepare($this->conn, $query);
+        $idCreature = $creature->getIdCreature();
+        mysqli_stmt_bind_param($stmt, 'i', $idCreature);
+        return $stmt->execute();
+    }
+
+
+}
+
+?>
