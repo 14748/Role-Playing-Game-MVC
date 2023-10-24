c3acdcbf8b03d7d94cad8747e445d249b908ccce
14748 <sproplayerxd@gmail.com>
2023-10-24 19:28:14 +0200

    feat(controller): add creature controller for inserting
    This commit adds a controller for insert clauses into Creatures. This controller is triggered by /creature/insert.php when form is filled and button is clicked performing post request to controller

---
+++ app/public/views/creature/insert.php
@@ -0,0 +1,13 @@
+<?php
+?>
+
+<form method="post" action="../../../controllers/creature/insertController.php">
+    <input name="id">
+    <input name="name">
+    <input name="desc">
+    <input name="avatar">
+    <input name="power">
+    <input name="life">
+    <input name="weapon">
+    <button type="submit"></button>
+</form>
